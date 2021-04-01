794851bfe739c7161d2406360980f289292c0385
Atul Chaudhari <atulchaudhari09@gmail.com>
2021-04-01 14:04:03 +0530

    test suites

--- Test Suites/dashboard.ts
+++ Test Suites/dashboard.ts
@@ -11,4 +11,10 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>fe379a76-bedd-45a9-bb82-7cb420e2fdc4</testSuiteGuid>
+   <testCaseLink>
+      <guid>63ee9300-2f4e-47e1-bcb7-b450681fd9fa</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/dashboard</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
