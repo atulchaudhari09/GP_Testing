c8ba6bd2ede687796c85ab95ea649903018b7999
Atul Chaudhari <atulchaudhari09@gmail.com>
2021-04-01 09:10:28 +0530

    Test Case

---
+++ Test Suites/dashboard.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>dashboard</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>fe379a76-bedd-45a9-bb82-7cb420e2fdc4</testSuiteGuid>
+</TestSuiteEntity>
